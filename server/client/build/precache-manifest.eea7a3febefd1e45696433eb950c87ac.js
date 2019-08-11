309c9f3ca90f7aa5916d81f45662e0782d55bd3a
Lennert Van Sever <lennert.vansever@borealisgroup.com>
2019-08-11 20:56:19 +0200

    include latest build

---
+++ server/client/build/precache-manifest.eea7a3febefd1e45696433eb950c87ac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8360cb05f7f14244f6a72733e5d90a10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dc85fbf94b51df427b2",
+    "url": "/static/js/2.6c20381e.chunk.js"
+  },
+  {
+    "revision": "8828ac712d33ecf50f24",
+    "url": "/static/js/main.15de4bfa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0eddca80f82c2699dcdfdd0957a15ded",
+    "url": "/static/media/Arrow.0eddca80.svg"
+  },
+  {
+    "revision": "359008406f79efa40cfce719143fcfbf",
+    "url": "/static/media/Cross.35900840.svg"
+  },
+  {
+    "revision": "6fce2d2b9ca742c20f6f93028f2a01fb",
+    "url": "/static/media/GitHub.6fce2d2b.svg"
+  },
+  {
+    "revision": "c7531a9044f40378ef65f2b47512f56f",
+    "url": "/static/media/Reload.c7531a90.svg"
+  },
+  {
+    "revision": "e36cb75ae364cd24ac03da1d1b2196e9",
+    "url": "/static/media/Search.e36cb75a.svg"
+  },
+  {
+    "revision": "cf311935f0271bd87409621183fb6027",
+    "url": "/static/media/Share.cf311935.svg"
+  }
+]);
